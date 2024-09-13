605d6b045ea04f2e69ec3c0fd5cbccca0f46baeb
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-09-13 17:38:24 +0700

    Update main-tes.js

--- main-tes.js
+++ main-tes.js
@@ -1,76 +1,76 @@
-require('dotenv').config();
-const fs = require('fs').promises;
-const path = require('path');
-const axios = require('axios');
-const colors = require('colors');
-const readline = require('readline');
-const { DateTime } = require('luxon');
-const fetch = require('node-fetch'); // Tambahkan ini untuk import node-fetch
+require('dotenv').config(); // Load environment variables
+const fs = require("fs").promises;
+const path = require("path");
+const axios = require("axios");
+const colors = require("colors");
+const readline = require("readline");
+const { DateTime } = require("luxon");
 
 class Fintopio {
   constructor() {
-    this.baseUrl = 'https://fintopio-tg.fintopio.com/api';
+    this.baseUrl = "https://fintopio-tg.fintopio.com/api";
     this.headers = {
-      Accept: 'application/json, text/plain, */*',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9',
-      Referer: 'https://fintopio-tg.fintopio.com/',
-      'Sec-Ch-Ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-      'Sec-Ch-Ua-Mobile': '?1',
-      'Sec-Ch-Ua-Platform': '"Android"',
-      'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36',
+      Accept: "application/json, text/plain, */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+      Referer: "https://fintopio-tg.fintopio.com/",
+      "Sec-Ch-Ua":
+        '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+      "Sec-Ch-Ua-Mobile": "?1",
+      "Sec-Ch-Ua-Platform": '"Android"',
+      "User-Agent":
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36",
     };
   }
 
-  log(msg, color = 'white') {
-    console.log(msg[color]);
-    this.sendTelegramMessage(msg); // Kirim pesan ke bot Telegram
-  }
-
-  async sendTelegramMessage(message) {
-    const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  async sendToTelegram(message) {
+    const token = process.env.TELEGRAM_BOT_TOKEN;
     const chatId = process.env.TELEGRAM_CHAT_ID;
-
-    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
     const payload = {
       chat_id: chatId,
       text: message,
+      parse_mode: "HTML" // Optional: to format the message
     };
 
     try {
-      await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers: { 'Content-Type': 'application/json' },
-      });
+      await axios.post(url, payload);
     } catch (error) {
-      console.error(`Error mengirim pesan ke Telegram: ${error.message}`);
+      console.error("Error sending message to Telegram:", error.message);
     }
   }
 
-  async waitWithCountdown(seconds, msg = 'melanjutkan') {
-    const spinners = ['|', '/', '-', '\\'];
+  async log(msg, color = "white", sendToTelegram = false) {
+    console.log(msg[color]);
+
+    if (sendToTelegram) {
+      await this.sendToTelegram(msg);
+    }
+  }
+
+  async waitWithCountdown(seconds, msg = 'continue') {
+    const spinners = ["|", "/", "-", "\\"];
     let i = 0;
     for (let s = seconds; s >= 0; s--) {
       readline.cursorTo(process.stdout, 0);
       process.stdout.write(
-        `${spinners[i]} Menunggu ${s} detik untuk ${msg} ${spinners[i]}`.cyan
+        `${spinners[i]} Waiting ${s} seconds to ${msg} ${spinners[i]}`.cyan
       );
       i = (i + 1) % spinners.length;
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
-    console.log('');
+    console.log("");
   }
 
   async auth(userData) {
     const url = `${this.baseUrl}/auth/telegram`;
-    const headers = { ...this.headers, Webapp: 'true' };
+    const headers = { ...this.headers, Webapp: "true" };
 
     try {
       const response = await axios.get(`${url}?${userData}`, { headers });
       return response.data.token;
     } catch (error) {
-      this.log(`Kesalahan otentikasi: ${error.message}`, 'red');
+      await this.log(`Authentication error: ${error.message}`, "red", true);
       return null;
     }
   }
@@ -80,14 +80,14 @@ class Fintopio {
     const headers = {
       ...this.headers,
       Authorization: `Bearer ${token}`,
-      Webapp: 'false, true',
+      Webapp: "false, true",
     };
 
     try {
       const response = await axios.get(url, { headers });
       return response.data;
     } catch (error) {
-      this.log(`Kesalahan mengambil profil: ${error.message}`, 'red');
+      await this.log(`Error fetching profile: ${error.message}`, "red", true);
       return null;
     }
   }
@@ -97,14 +97,14 @@ class Fintopio {
     const headers = {
       ...this.headers,
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     try {
       await axios.post(url, {}, { headers });
-      this.log('Check-in harian berhasil!', 'green');
+      await this.log("Daily check-in successful!", "green", true);
     } catch (error) {
-      this.log(`Kesalahan check-in harian: ${error.message}`, 'red');
+      await this.log(`Daily check-in error: ${error.message}`, "red", true);
     }
   }
 
@@ -119,7 +119,7 @@ class Fintopio {
       const response = await axios.get(url, { headers });
       return response.data;
     } catch (error) {
-      this.log(`Kesalahan mengambil status farming: ${error.message}`, 'red');
+      await this.log(`Error fetching farming state: ${error.message}`, "red", true);
       return null;
     }
   }
@@ -129,7 +129,7 @@ class Fintopio {
     const headers = {
       ...this.headers,
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     try {
@@ -140,13 +140,13 @@ class Fintopio {
         const finishTime = DateTime.fromMillis(finishTimestamp).toLocaleString(
           DateTime.DATETIME_FULL
         );
-        this.log(`Memulai farming...`, 'yellow');
-        this.log(`Waktu selesai farming: ${finishTime}`, 'green');
+        await this.log(`Starting farm...`, "yellow", true);
+        await this.log(`Farming completion time: ${finishTime}`, "green", true);
       } else {
-        this.log('Tidak ada waktu penyelesaian yang tersedia.', 'yellow');
+        await this.log("No completion time available.", "yellow", true);
       }
     } catch (error) {
-      this.log(`Kesalahan memulai farming: ${error.message}`, 'red');
+      await this.log(`Error starting farming: ${error.message}`, "red", true);
     }
   }
 
@@ -155,30 +155,30 @@ class Fintopio {
     const headers = {
       ...this.headers,
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     try {
       await axios.post(url, {}, { headers });
-      this.log('Farming berhasil diklaim!', 'green');
+      await this.log("Farm claimed successfully!", "green", true);
     } catch (error) {
-      this.log(`Kesalahan mengklaim farming: ${error.message}`, 'red');
+      await this.log(`Error claiming farm: ${error.message}`, "red", true);
     }
   }
 
-  async getDiamondInfo(token) {
+  async getDiamondInfo(token){
     const url = `${this.baseUrl}/clicker/diamond/state`;
     const headers = {
-      ...this.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+        ...this.headers,
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
     };
 
     try {
-      const response = await axios.get(url, { headers });
-      return response.data;
+        const response = await axios.get(url, { headers });
+        return response.data;
     } catch (error) {
-      this.log(`Kesalahan mengambil status diamond: ${error.message}`, 'red');
+      await this.log(`Error fetching diamond state: ${error.message}`, "red", true);
       return null;
     }
   }
@@ -186,33 +186,33 @@ class Fintopio {
   async claimDiamond(token, diamondNumber, totalReward) {
     const url = `${this.baseUrl}/clicker/diamond/complete`;
     const headers = {
-      ...this.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+        ...this.headers,
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
     };
-    const payload = { diamondNumber: diamondNumber };
+    const payload = { "diamondNumber": diamondNumber };
 
     try {
-      await axios.post(url, payload, { headers });
-      this.log(`Berhasil mengklaim ${totalReward} diamond!`, 'green');
+        await axios.post(url, payload, { headers });
+        await this.log(`Success claim ${totalReward} diamonds!`, "green", true);
     } catch (error) {
-      this.log(`Kesalahan mengklaim diamond: ${error.message}`, 'red');
+      await this.log(`Error claiming Diamond: ${error.message}`, "red", true);
     }
   }
 
   async getTask(token) {
     const url = `${this.baseUrl}/hold/tasks`;
     const headers = {
-      ...this.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+        ...this.headers,
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
     };
 
     try {
-      const response = await axios.get(url, { headers });
-      return response.data;
+        const response = await axios.get(url, { headers });
+        return response.data;
     } catch (error) {
-      this.log(`Kesalahan mengambil status tugas: ${error.message}`, 'red');
+      await this.log(`Error fetching task state: ${error.message}`, "red", true);
       return null;
     }
   }
@@ -220,95 +220,111 @@ class Fintopio {
   async startTask(token, taskId, slug) {
     const url = `${this.baseUrl}/hold/tasks/${taskId}/start`;
     const headers = {
-      ...this.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json; charset=utf-8',
-      origin: 'https://fintopio-tg.fintopio.com',
+        ...this.headers,
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json; charset=utf-8",
+        "origin": "https://fintopio-tg.fintopio.com"
     };
     try {
-      await axios.post(url, {}, { headers });
-      this.log(`Memulai tugas ${slug}!`, 'green');
+        await axios.post(url, {}, { headers });
+        await this.log(`Starting task ${slug}!`, "green", true);
     } catch (error) {
-      this.log(`Kesalahan memulai tugas: ${error.message}`, 'red');
+      await this.log(`Error starting task: ${error.message}`, "red", true);
     }
   }
 
   async claimTask(token, taskId, slug, rewardAmount) {
     const url = `${this.baseUrl}/hold/tasks/${taskId}/claim`;
     const headers = {
-      ...this.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json; charset=utf-8',
-      origin: 'https://fintopio-tg.fintopio.com',
+        ...this.headers,
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json; charset=utf-8",
+        "origin": "https://fintopio-tg.fintopio.com"
     };
-
     try {
-      await axios.post(url, {}, { headers });
-      this.log(`Mengklaim tugas ${slug} senilai ${rewardAmount} berhasil!`, 'green');
+        await axios.post(url, {}, { headers });
+        await this.log(`Task ${slug} complete, reward ${rewardAmount} diamonds!`, "green", true);
     } catch (error) {
-      this.log(`Kesalahan mengklaim tugas: ${error.message}`, 'red');
+      await this.log(`Error claiming task: ${error.message}`, "red", true);
     }
+  }
+
+  extractFirstName(userData) {
+    try {
+      const userPart = userData.match(/user=([^&]*)/)[1];
+      const decodedUserPart = decodeURIComponent(userPart);
+      const userObj = JSON.parse(decodedUserPart);
+      return userObj.first_name || "Unknown";
+    } catch (error) {
+      this.log(`Error extracting first_name: ${error.message}`, "red", true);
+      return "Unknown";
+    }
+  }
+
+  calculateWaitTime(firstAccountFinishTime) {
+    if (!firstAccountFinishTime) return null;
+
+    const now = DateTime.now();
+    const finishTime = DateTime.fromMillis(firstAccountFinishTime);
+    const duration = finishTime.diff(now);
+
+    return duration.as("milliseconds");
   }
 
   async main() {
     while (true) {
-      const dataFile = path.join(__dirname, 'data.txt');
-      const data = await fs.readFile(dataFile, 'utf8');
-      const users = data.split('\n').filter(Boolean);
+      const dataFile = path.join(__dirname, "data.txt");
+      const data = await fs.readFile(dataFile, "utf8");
+      const users = data.split("\n").filter(Boolean);
 
       let firstAccountFinishTime = null;
 
       for (let i = 0; i < users.length; i++) {
         const userData = users[i];
         const first_name = this.extractFirstName(userData);
-        console.log(
-          `${'='.repeat(5)} Akun ${i + 1} | ${first_name.green} ${'='.repeat(
-            5
-          )}`.blue
+        await this.log(
+          `${"=".repeat(5)} Account ${i + 1} | ${first_name} ${"=".repeat(5)}`,
+          "blue",
+          true // Send this log to Telegram
         );
+
         const token = await this.auth(userData);
         if (token) {
-          this.log('Login berhasil!', 'green');
+          await this.log(`Login successful!`, "green", true);
+
           const profile = await this.getProfile(token);
           if (profile) {
             const balance = profile.balance;
-            this.log(`Saldo: ${balance}`, 'green');
+            await this.log(`Balance: ${balance}`, "green", true);
 
             await this.checkInDaily(token);
 
             const diamond = await this.getDiamondInfo(token);
-            if(diamond.state === 'available') {
-              await this.waitWithCountdown(Math.floor(Math.random() * (21 - 10)) + 10, 'klaim Diamond');
+            if (diamond.state === 'available') {
+              await this.waitWithCountdown(Math.floor(Math.random() * (21 - 10)) + 10, 'claim Diamonds');
               await this.claimDiamond(token, diamond.diamondNumber, diamond.settings.totalReward);
             } else {
-                const nextDiamondTimeStamp = diamond.timings.nextAt;
-                if(nextDiamondTimeStamp) {
-                    const nextDiamondTime = DateTime.fromMillis(
-                        nextDiamondTimeStamp
-                    ).toLocaleString(DateTime.DATETIME_FULL);
-                    this.log(`Waktu Diamond berikutnya: ${nextDiamondTime}`, 'green');
+              const nextDiamondTimeStamp = diamond.timings.nextAt;
+              if (nextDiamondTimeStamp) {
+                const nextDiamondTime = DateTime.fromMillis(nextDiamondTimeStamp).toLocaleString(DateTime.DATETIME_FULL);
+                await this.log(`Next Diamond time: ${nextDiamondTime}`, 'green', true);
 
-                    if (i === 0) {
-                        firstAccountFinishTime = nextDiamondTimeStamp;
-                    }
+                if (i === 0) {
+                  firstAccountFinishTime = nextDiamondTimeStamp;
                 }
+              }
             }
 
             const farmingState = await this.getFarmingState(token);
 
             if (farmingState) {
-              if (farmingState.state === 'idling') {
+              if (farmingState.state === "idling") {
                 await this.startFarming(token);
-              } else if (
-                farmingState.state === 'farmed' ||
-                farmingState.state === 'farming'
-              ) {
+              } else if (farmingState.state === "farmed" || farmingState.state === "farming") {
                 const finishTimestamp = farmingState.timings.finish;
                 if (finishTimestamp) {
-                  const finishTime = DateTime.fromMillis(
-                    finishTimestamp
-                  ).toLocaleString(DateTime.DATETIME_FULL);
-                  this.log(`Waktu selesai farming: ${finishTime}`, 'green');
+                  const finishTime = DateTime.fromMillis(finishTimestamp).toLocaleString(DateTime.DATETIME_FULL);
+                  await this.log(`Farming completion time: ${finishTime}`, "green", true);
 
                   const currentTime = DateTime.now().toMillis();
                   if (currentTime > finishTimestamp) {
@@ -321,18 +337,18 @@ class Fintopio {
 
             const taskState = await this.getTask(token);
 
-            if(taskState) {
-                for (const item of taskState.tasks) {
-                    if(item.status === 'available') {
-                        await this.startTask(token, item.id, item.slug);
-                    } else if(item.status === 'verified') {
-                        await this.claimTask(token, item.id, item.slug, item.rewardAmount);
-                    } else if(item.status === 'in-progress') {
-                        continue;
-                    } else {
-                        this.log(`Memverifikasi tugas ${item.slug}!`, 'green');
-                    }
+            if (taskState) {
+              for (const item of taskState.tasks) {
+                if (item.status === 'available') {
+                  await this.startTask(token, item.id, item.slug);
+                } else if (item.status === 'verified') {
+                  await this.claimTask(token, item.id, item.slug, item.rewardAmount);
+                } else if (item.status === 'in-progress') {
+                  continue;
+                } else {
+                  await this.log(`Verifying task ${item.slug}!`, "green", true);
                 }
+              }
             }
           }
         }
@@ -342,22 +358,10 @@ class Fintopio {
       if (waitTime && waitTime > 0) {
         await this.waitWithCountdown(Math.floor(waitTime / 1000));
       } else {
-        this.log('Tidak ada waktu tunggu yang valid, melanjutkan loop segera.', 'yellow');
+        await this.log("No valid wait time, continuing loop immediately.", "yellow", true);
         await this.waitWithCountdown(5);
       }
     }
-  }
-
-  calculateWaitTime(firstAccountFinishTime) {
-    if (!firstAccountFinishTime) return 0;
-
-    const currentTime = DateTime.now().toMillis();
-    return firstAccountFinishTime - currentTime;
-  }
-
-  extractFirstName(userData) {
-    // Implementasikan logika ekstraksi nama depan dari userData
-    return userData.split(' ')[0]; // Contoh implementasi sederhana
   }
 }
 
